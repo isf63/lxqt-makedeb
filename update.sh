7cd4044a8f0991207f6b08a071b3aa1289934b8a
Ian Farrell <ianfarrell47@gmail.com>
2025-08-30 13:14:35 -0400

    Update

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+is_installed() {
+	dpkg -l "$1" 1>/dev/null 2>/dev/null
+}
+
 local_version() {
-	apt-cache policy $1 | grep "Installed" | sed 's/  Installed: //g'
+	apt-cache policy "$1" | grep "Installed" | sed "s/  Installed: //g"
+}
+
+local_hash() {
+	echo $1 | sed 's/.*\..//; s/..$//'
 }
 
 remote_hash() {
@@ -16,20 +24,19 @@ for pkg in pkgbuild/*; do
 
 	_pkgname="$(basename $pkg)"
 
-	dpkg -l "$_pkgname" 1>/dev/null 2>/dev/null
-	if [[ $? != 0 ]]; then
+	if ! is_installed "$_pkgname"; then
+		echo -e "[NOT INSTALLED]\t$_pkgname"
 		continue
 	fi
 
 	_local_ver="$(local_version $_pkgname)"
-	_local_hash="$(echo $_local_ver | sed 's/.*\..//; s/..$//')"
-
+	_local_hash="$(local_hash $_local_ver)"
 	_remote_hash="$(remote_hash $_pkgname)"
 
 	if [[ "$_remote_hash" == "$_local_hash"* ]]; then
 		echo -e "[UP-TO-DATE]\t$_pkgname"
 	else
-		echo -e "[ UPDATING ]\t$_pkgname\t($_local_ver -> ${_remote_hash:0:8})"
+		echo -e "[UPDATING]\t$_pkgname\t($_local_ver -> ${_remote_hash:0:8})"
 
 		find "$pkg" -maxdepth 1 ! -wholename "$pkg" ! -name "PKGBUILD" -exec sudo rm -rf {} +
 		./install.sh "$_pkgname"
